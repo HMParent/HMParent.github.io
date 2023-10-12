94f534242bc299a0726dcdd56b03005ae6e27cec
HMParent <hunterp395@gmail.com>
2023-10-11 21:42:28 -0400

    Incremental Update 2
    Fixed bugs in index.html and main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var gameData = {
     goldPerClick: 1,
     goldPerClickCost: 10
   }
-
-function mineGold() {
+  
+  function mineGold() {
     gameData.gold += gameData.goldPerClick
     document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
   }
-
-function buyGoldPerClick() {
+  
+  function buyGoldPerClick() {
     if (gameData.gold >= gameData.goldPerClickCost) {
       gameData.gold -= gameData.goldPerClickCost
       gameData.goldPerClick += 1
@@ -18,3 +18,7 @@ function buyGoldPerClick() {
       document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
     }
   }
+  
+  var mainGameLoop = window.setInterval(function() {
+    mineGold()
+  }, 1000)
